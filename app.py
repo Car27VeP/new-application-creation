3363f4180cf0cf1632d7702f910ac3f5e244ad25
Car27VeP <carlos.pazaran@outlook.com>
2024-05-06 22:18:19 -0600

    cambio por carlos

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import streamlit as st
 import time
 
 # estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
+
 if 'experiment_no' not in st.session_state:
     st.session_state['experiment_no'] = 0
 
 if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
-
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+    
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
@@ -33,7 +34,7 @@ def toss_coin(n):
     return mean
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-start_button = st.button('Ejecutar')
+start_button = st.button('Start')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
